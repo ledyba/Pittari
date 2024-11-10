bbbad15a66444721791ca6bf714ec90e78340052
Kaede Fujisaki <kaede@hexe.net>
2024-11-10 15:57:08 +0900

    Build script

--- build.rs
+++ build.rs
@@ -1,8 +1,9 @@
 use std::process::Command;
+use base64::prelude::*;
+
 fn main() {
   // note: add error checking yourself.
-  let output = Command::new("git").args(&["log", "-1"]).output().expect("Failed to execute: git rev-parse HEAD");
-  let git_rev = String::from_utf8(output.stdout).unwrap();
-  println!("cargo:rustc-env=GIT_REV={}", git_rev);
+  let git_rev = Command::new("git").args(&["log", "-1"]).output().expect("Failed to execute: git log -1");
+  println!("cargo:rustc-env=GIT_REV={}", BASE64_STANDARD.encode(git_rev.stdout));
   println!("cargo:rustc-env=BUILD_AT={}", chrono::Local::now().to_rfc3339_opts(chrono::SecondsFormat::Secs, false));
 }
