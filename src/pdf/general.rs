884e4eab5e4d4f5b64b8205bb4e7a33da879affd
Kaede Fujisaki <kaede@hexe.net>
2025-08-07 15:54:19 +0900

    Force RGB8

--- src/pdf/general.rs
+++ src/pdf/general.rs
@@ -5,7 +5,7 @@ pub fn build_pdf(
   let image = image::load_from_memory(&image_data)?;
   let mut data = Vec::<u8>::new();
   let codec = image::codecs::jpeg::JpegEncoder::new_with_quality(&mut data, 100);
-  image.write_with_encoder(codec)?;
+  image.to_rgb8().write_with_encoder(codec)?;
 
   super::jpeg::build_pdf(spec, data)
 }
